eb0e652ed755ee538253de26c426410de2930039
masch <the.masch@gmail.com>
2022-11-21 12:04:07 -0300

    Support ignore TG message on sandbox

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -6,7 +6,7 @@ import { Challenge, getChallengeInfo } from "./poh";
 import * as telegram from "./telegram";
 
 async function main(configuration: AppConfig) {
-  console.info("Starting with configuration:", configuration);
+  console.info("== Starting with configuration :", configuration);
 
   const provider = new ethers.providers.JsonRpcProvider(
     configuration.infuraConfig.url
@@ -28,24 +28,8 @@ async function main(configuration: AppConfig) {
     return tgMessageData;
   };
 
-  console.info("Get challenge info");
-  let demoChallengeInfo: Challenge;
-  if (!configuration.serverConfig.mockChallengeInfo) {
-    demoChallengeInfo = await getChallengeInfo(
-      "0x2ee440fd2b5da461a608843f0ad880496d725857".toLowerCase(),
-      0,
-      0
-    );
-  } else {
-    demoChallengeInfo = {
-      name: "Paula",
-      challengeReason:
-        '"The video quality should be at least 360p..." This video is 352 x 640, which is less than 360p. As set precedent by cases 674 and 673, size does matter!',
-      pohProfileLink:
-        "https://app.proofofhumanity.id/profile/0xf8fd6890836e2051112b749a9f981b0827050a47",
-      klerosCaseLink: `https://resolve.kleros.io/cases/755`,
-    };
-  }
+  console.info("== Get challenge info ==");
+  const demoChallengeInfo = await fetchChallengeInfo();
 
   console.info("== Demo challenge telegram ==");
   const tgMessage = telegram.makeMessage(
@@ -53,12 +37,15 @@ async function main(configuration: AppConfig) {
   );
   console.log(tgMessage);
 
-  telegram
-    .postMessage(configuration.telegramConfig)(
-      infoToTelegramMessageData(demoChallengeInfo)
-    )
-    .then((data) => console.log("Message id created: " + data.id))
-    .catch((error) => console.log("Could not send message: " + error));
+  if (configuration.serverConfig.postMessageToTelegramOnSanbox) {
+    console.info("== Posting message on telegram ==");
+    telegram
+      .postMessage(configuration.telegramConfig)(
+        infoToTelegramMessageData(demoChallengeInfo)
+      )
+      .then((data) => console.log("Message id created: " + data.id))
+      .catch((error) => console.log("Could not send message: " + error));
+  }
 
   poh.on(poh.filters.AddSubmission(), (_, __, event) => {
     console.info("Got submission add event:", event);
@@ -77,6 +64,25 @@ async function main(configuration: AppConfig) {
       telegram.makeMessage(infoToTelegramMessageData(demoChallengeInfo))
     );
   });
+
+  async function fetchChallengeInfo(): Promise<Challenge> {
+    if (!configuration.serverConfig.mockChallengeInfo) {
+      return getChallengeInfo(
+        "0x2ee440fd2b5da461a608843f0ad880496d725857".toLowerCase(),
+        0,
+        0
+      );
+    } else {
+      return {
+        name: "Paula",
+        challengeReason:
+          '"The video quality should be at least 360p..." This video is 352 x 640, which is less than 360p. As set precedent by cases 674 and 673, size does matter!',
+        pohProfileLink:
+          "https://app.proofofhumanity.id/profile/0xf8fd6890836e2051112b749a9f981b0827050a47",
+        klerosCaseLink: `https://resolve.kleros.io/cases/755`,
+      };
+    }
+  }
 }
 
 config.appConfigFromEnvironment().then(main).catch(console.error);
